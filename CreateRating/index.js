e54f706a086898942df113eaaec3d356d86effab
Vito Chin <vitochin@microsoft.com>
2021-11-16 16:37:05 +0800

    CreateRating

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -1,26 +1,19 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.log(req.body.locationName);
-    context.bindings.employeeDocument = JSON.stringify({
-        id: req.body.name + "-" + req.body.employeeId,
-        name: req.body.name,
-        employeeId: req.body.employeeId,
-        address: req.body.address
-    });
+    const rating = {
+        userId: req.body.userId,
+        productId: req.body.productId,
+        locationName: req.body.locationName,
+        rating: req.body.rating,
+        userNotes: req.body.userNotes
+    }
 
-    context.done();
-    
-
-    /*
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.bindings.outputDoc = JSON.stringify(rating);
 
     context.res = {
-        // status: 200,
-        body: responseMessage
+        status: 200,
+        body: rating
     };
-    */
+
 }
